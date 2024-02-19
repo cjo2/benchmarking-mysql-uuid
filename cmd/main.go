1a4f3fbeaee94d989883fa7125af6e5000868820
Christopher Docuyanan <christopher.docuyanan@segment.com>
2024-02-18 22:14:30 -0800

    write to db

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -20,7 +23,13 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			slog.Error("error trying to close db: ", err)
+		}
+	}(db)
 
 	err = db.Ping()
 	if err != nil {
@@ -28,5 +37,42 @@ func main() {
 	}
 
 	slog.Info("connected")
+
+	iterations := 100000
+
+	version := 1
+
+	ids := make([]string, iterations)
+
+	for i := 0; i < iterations; i++ {
+		if version == 1 {
+			val, err := uuid.NewUUID()
+			if err != nil {
+				slog.Error("Error creating UUID: ", err)
+			}
+			ids[i] = val.String()
+		}
+
+		if version == 4 {
+			val, err := uuid.NewRandom()
+			if err != nil {
+				slog.Error("Error creating UUID: ", err)
+			}
+			ids[i] = val.String()
+		}
+	}
+
+	start := time.Now()
+
+	for i := 0; i < iterations; i++ {
+		_, err = db.Exec("INSERT INTO TestTable (ID, Name) VALUES (?, ?)", ids[i], "")
+		if err != nil {
+			slog.Error("Error inserting row: ", err)
+		}
+	}
+
+	since := time.Since(start)
+
+	slog.Info(fmt.Sprintf("Time to insert rows: %s", since.String()))
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module benchmarking-mysql-uuid
 
 go 1.22
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
